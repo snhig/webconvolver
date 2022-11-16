7e3a29b5d48d6876f36c538207031caaeff1b4cf
SeanNH <s_higley@u.pacific.edu>
2022-11-16 14:47:32 -0800

    write to json from php script

--- scripts/read_samples.php
+++ scripts/read_samples.php
@@ -1,39 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script src="https://cdn.plot.ly/plotly-2.16.1.min.js"></script>
+    <link rel="stylesheet" href="../css/graph_style.css">
+    <!--    <script src="scripts/helper.js" type="text/javascript"></script>-->
+    <title>Convolver</title>
+</head>
 <?php
-
-include_once "wave_helper/wave_helper.php";
-include_once "wave_helper/fourrierTransform.php";
-#impulse response
-$resolution = 100;
-
-$ir = new Wave();
-$ir->setSteps($resolution);
-$ir->setFileName('../sample_files/ir_2.wav');
-$ir_data = $ir->getWaveformData();
-//echo "ir num channels: " . count($ir_data->getChannels()) . "\n";
-$ir_channels = $ir_data->getChannels()[0];
-$ir_samples = $ir_channels->getValues();
-
-#sample file
-$sm = new Wave();
-$sm->setSteps($resolution);
-$sm->setFileName('../sample_files/sample.wav');
-$sm_data = $sm->getWaveformData();
-//echo "sm num channels: " . count($sm_data->getChannels()) . "\n";
-$sm_channels = $sm_data->getChannels()[0];
-$sm_samples = $sm_channels->getValues();
-
-$convolved = fftConvolution($ir_samples, $sm_samples);
-//foreach ($convolved as $c_val) {
-//    echo $c_val . "\n";
-//}
-//write_waveform("convolved_audio.wav", $convolved, $resolution);
-
-// PERHAPS WE WRITE JSON HERE :))))
+//
+//include_once "wave_helper/wave_helper.php";
+//include_once "wave_helper/fourrierTransform.php";
+//#impulse response
+//$resolution = 100;
+//
+//$ir = new Wave();
+//$ir->setSteps($resolution);
+//$ir->setFileName('../sample_files/ir_2.wav');
+//$ir_data = $ir->getWaveformData();
+//
+//$ir_channels = $ir_data->getChannels()[0];
+//$ir_samples = $ir_channels->getValues();
+//
+//#sample file
+//$sm = new Wave();
+//$sm->setSteps($resolution);
+//$sm->setFileName('../sample_files/sample.wav');
+//$sm_data = $sm->getWaveformData();
+//
+//$sm_channels = $sm_data->getChannels()[0];
+//$sm_samples = $sm_channels->getValues();
+//
+//$convolved = fftConvolution($ir_samples, $sm_samples);
+//
+//// PERHAPS WE WRITE JSON HERE :))))
+//$file_out_contents['convolved'] = $convolved;
+//$file_out_contents['ir_data'] = $ir_samples;
+//$file_out_contents['sm_data'] = $sm_samples;
+//
+//$json = json_encode($file_out_contents);
+//file_put_contents("../uploads/wav_data.json", $json);
 
 ?>
 
-//$sample_header = fopen('../sample_files/WET_OUT.wav', 'r');
-//read_and_validate_WAV($sample_header);
-//print_wav_metadata($sample_header);
-//$byteArray = unpack('N*')
-//print_wav_metadata($sample_header);
+<body>
+<h1 class="welcome">VIEW</h1>
+
+<div id="tester" style="width:600px;height:250px;"
+
+
+</body>
+
+
+</html>
